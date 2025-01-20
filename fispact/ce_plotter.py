c6bc8dc92e5629305325c3b29c04780bdf3d08b0
louisbutt338 <louisbutt338@gmail.com>
2025-01-20 19:25:41 +0000

    cleaning up ce_plotter

--- fispact/ce_plotter.py
+++ fispact/ce_plotter.py
@@ -21,9 +21,8 @@ import math
 
 ################# DIRECTORY INPUTS #################
 
-image_directory = '/Users/ljb841@student.bham.ac.uk/fispact/WORKSHOP/uBB/analysis'
-calc_data_directory = f"{image_directory}/calculated_activities"
-exp_data_directory =  f"{image_directory}/experimental_activities"
+working_directory = '/Users/ljb841@student.bham.ac.uk/fispact/WORKSHOP/uBB/analysis'
+exp_results_directory = '/Users/ljb841@student.bham.ac.uk/gamma_spec/proton_hpge/experimental_activities'
 
 ################# FLUX NORMALISATION CALCS ################# 
 
@@ -42,233 +41,28 @@ print(f"max n/s from lithium at 10uA proton current: {estimated_10ua_li_n_per_s(
 
 ################# LIST OF ANALYSED ISOTOPES ################# 
 
-isotope_list = [
-'$^{56}Mn$',
-'$^{196}Au$',
-'$^{198}Au$', 
-'$^{24}Na$',
-'$^{65}Ni$',
-'$^{64}Cu$',
-'$^{111m}Cd$',
-'$^{115}Cd$',
-'$^{115m}In$',
-'$^{116m}In$',
-'$^{57}Ni$',
-'$^{165}Dy$', 
-'$^{157}Dy$', 
-'$^{92m}Nb$',
-'$^{7}Be$ *'] 
+experiment = 'proton_march24'
+model_dataset = 'seperated_foils'
 
-################# CALCULATED RESULTS FOR ALL THREE LIBRARIES ################# 
+experimental_analysis_lib = 'interspec'
 
-# calibration experiment model results (iron foil)
+model_results_path = f"{working_directory}/calculated_activities/{experiment}/{model_dataset}.json"
+model_results_data = json.load(open(model_results_path))
+isotope_list = model_results_data["isotope_data"]["isotope_list"]
+foil_weight_normalisation = model_results_data["isotope_data"]["associated_foil_weights"]
 
-# calculated_endfb8_activities = [
-# 1.4440*8.976E+02 ,
-# 0.1498*9.205E+01 ,
-# 0.1498*3.86E+03 ,
-# 0.2334*4.504E+02 ,
-# 0.7877*5.596E+01 ,
-# 0.7877*7.597E+02 ,
-# 1e6              , # reaction missing
-# 0.1328*1.003E+02 ,
-# 1e6              , # reaction missing
-# 1e6 , # reaction missing in116m product 
-# 1.5845*5.830E+00 ,
-# 0.0435*1.334E+04 ,
-# 0.0435*2.183E+01 ,
-# 1e6              , # reaction missing
-# (4.110146e-02*1.500E+06) + (1.342088e-02*4.948e6)]
-# 
-# calculated_endfb8_uncertainties = [
-# 1.4440*1.71E+01,
-# 0.1498*0.00E+00,
-# 0.1498*7.79E+01,
-# 0.2334*0.00E+00,
-# 0.7877*0.00E+00,
-# 0.7877*0.00E+00,
-# 0.1328*0.00E+00,
-# 0.1328*0.00E+00,
-# 0.6452*0.00E+00,
-# 0.6452*0.00E+00,
-# 1.5845*1.11E-01,
-# 0.0435*0.00E+00,
-# 0.0435*0.00E+00,
-# 0.7317*0.00E+00,
-# 4.110146e-02*0.00E+00]
-# 
-# calculated_irdff2_activities = [
-# 1.4440*8.972E+02,
-# 0.1498*9.350E+01,
-# 0.1498*4.306E+03,
-# 0.5*0.2334*9.043E+02, #    only half the Na24 generated here is from (n,a), hence multiplication factor
-# 1e6,        #    reaction missing
-# 0.7877*7.788E+02,
-# 1e6,        #    reaction missing
-# 1e6,        #    reaction missing
-# 1e6*0.6452*3210.0877645615906,        #    reaction missing
-# 0.6452*0.33334*7.031E+05,
-# 1.5845*5.584E+00,
-# 1e6,        #   reaction missing,
-# 1e6,        #   reaction missing,
-# 1e6*0.7317*20.127675759159207,        #   reaction missing]
-# 1e6]
-# 
-# calculated_irdff2_uncertainties = [
-# 1.4440*0,
-# 0.1498*0,
-# 0.1498*0,
-# 0.2334*0,
-# 0.7877*0,
-# 0.7877*0,
-# 0.1328*0,
-# 0.1328*0,
-# 0.6452*0,
-# 0.6452*0,
-# 1.5845*0,
-# 0.0435*0,
-# 0.0435*0,
-# 0.7317*0,
-# 4.110146e-02*0]
-# 
-# calculated_tendl21_activities = [
-# 1.4440*8.920E+02 ,
-# 0.1498*8.845E+01 ,
-# 0.1498*3.594E+03 ,
-# 0.2334*4.503E+02 ,
-# 0.7877*8.772E+01 ,
-# 0.7877*7.645E+02 ,
-# 0.1328*3.870E+03 ,
-# 0.1328*7.802E+01 ,
-# 0.6452*4.379E+03 ,
-# 0.6452*2.769E+05 ,
-# 1.5845*5.624E+00 ,
-# 0.0435*1.299E+04 ,
-# 0.0435*2.406E+01 ,
-# 0.7317*2.479E+01 ,
-# (4.110146e-02*2.422E+06) + (1.342088e-02*2.123e7)]
-# 
-# calculated_tendl21_uncertainties = [
-# 1.4440*9.14E+01 ,
-# 0.1498*3.71E+00 ,
-# 0.1498*5.34E+01 ,
-# 0.2334*0.00E+00 ,
-# 0.7877*6.44E+00 ,
-# 0.7877*3.22E+01 ,
-# 0.1328*1.54E+02 ,
-# 0.1328*5.50E+00 ,
-# 0.6452*2.03E+02 ,
-# 0.6452*7.51E+03 ,
-# 1.5845*8.51E-01 ,
-# 0.0435*9.36E+02 ,
-# 0.0435*1.78E+00 ,
-# 0.7317*1.50E+00 ,
-# (4.110146e-02*1.96E+06) + (1.342088e-02*2.94e6)]
+calculated_endfb8_activities = model_results_data["endfb8"]["activity"]
+calculated_endfb8_uncertainties = model_results_data["endfb8"]["uncertainty"]
+calculated_irdff2_activities = model_results_data["irdff2"]["activity"]
+calculated_irdff2_uncertainties = model_results_data["irdff2"]["uncertainty"]
+calculated_tendl21_activities = model_results_data["tendl21"]["activity"]
+calculated_tendl21_uncertainties = model_results_data["tendl21"]["uncertainty"]
 
 
-# calibration experiment model results (seperated foils)
-
-calculated_endfb8_activities = [
-1.4440*9.157E+02 ,
-0.1498*8.255E+01 ,
-0.1498*1.683E+03 ,
-0.2334*4.290E+02 ,
-0.7877*5.498E+01 ,
-0.7877*6.946E+02 ,
-1e6              , # reaction missing
-0.1328*8.556E+01 ,
-1e6              , # reaction missing
-1e6 , # reaction missing in116m product 
-1.5845*5.333E+00 ,
-0.0435*1.010E+04 ,
-0.0435*2.487E+01 ,
-1e6              , # reaction missing
-(4.110146e-02*1.500E+06) + (1.342088e-02*4.948e6)]
-
-calculated_endfb8_uncertainties = [
-1.4440*1.75E+01,
-0.1498*0.00E+00,
-0.1498*2.62E+01,
-0.2334*0.00E+00,
-0.7877*0.00E+00,
-0.7877*0.00E+00,
-0.1328*0.00E+00,
-0.1328*0.00E+00,
-0.6452*0.00E+00,
-0.6452*0.00E+00,
-1.5845*1.01E-01,
-0.0435*0.00E+00,
-0.0435*0.00E+00,
-0.7317*0.00E+00,
-4.110146e-02*0.00E+00]
-
-calculated_irdff2_activities = [
-1.4440*9.152E+02,
-0.1498*8.385E+01,
-0.1498*1.546E+03,
-0.5*0.2334*8.614E+02, #    only half the Na24 generated here is from (n,a), hence multiplication factor
-1e6,        #    reaction missing
-0.7877*7.181E+02,
-1e6,        #    reaction missing
-1e6,        #    reaction missing
-1e6*0.6452*3210.0877645615906,        #    reaction missing
-0.6452*0.33334*9.133E+05,
-1.5845*5.104E+00,
-1e6,        #   reaction missing,
-1e6,        #   reaction missing,
-1e6*0.7317*20.127675759159207,        #   reaction missing]
-1e6]
-
-calculated_irdff2_uncertainties = [
-1.4440*0,
-0.1498*0,
-0.1498*0,
-0.2334*0,
-0.7877*0,
-0.7877*0,
-0.1328*0,
-0.1328*0,
-0.6452*0,
-0.6452*0,
-1.5845*0,
-0.0435*0,
-0.0435*0,
-0.7317*0,
-4.110146e-02*0]
-
-calculated_tendl21_activities = [
-1.4440*9.099E+02 ,
-0.1498*7.932E+01 ,
-0.1498*1.418E+03 ,
-0.2334*4.289E+02 ,
-0.7877*8.617E+01 ,
-0.7877*7.025E+02 ,
-0.1328*3.358E+03 ,
-0.1328*6.376E+01 ,
-0.6452*3.784E+03 ,
-0.6452*3.655E+05 ,
-1.5845*5.141E+00 ,
-0.0435*9.821E+03 ,
-0.0435*2.664E+01 ,
-0.7317*2.123E+01 ,
-(4.110146e-02*2.422E+06) + (1.342088e-02*2.123e7)]
-
-calculated_tendl21_uncertainties = [
-1.4440*9.32E+01 ,
-0.1498*3.36E+00 ,
-0.1498*4.64E+01 ,
-0.2334*0.00E+00 ,
-0.7877*6.33E+00 ,
-0.7877*3.16E+01 ,
-0.1328*1.34E+02 ,
-0.1328*4.76E+00 ,
-0.6452*1.75E+02 ,
-0.6452*9.79E+03 ,
-1.5845*7.78E-01 ,
-0.0435*7.32E+02 ,
-0.0435*1.81E+00 ,
-0.7317*1.28E+00 ,
-(4.110146e-02*1.96E+06) + (1.342088e-02*2.94e6)]
+exp_results_path =  f"{exp_results_directory}/{experimental_analysis_lib}_activities.json" 
+exp_results_data = json.load(open(exp_results_path))
+experimental_activities = exp_results_data["activity"]
+experimental_uncertainties = exp_results_data["uncertainty"]
 
 ################# CALCULATED UNCERTAINTIES FUNCTION ################# 
 
@@ -280,118 +74,13 @@ def total_calculated_uncerts(calc_fispact_uncerts,calc_activities):
         calculated_uncertainties_frac.append(np.sqrt((squares_calc_uncerts)))
     return calculated_uncertainties_frac
 
-################# EXPERIMENTAL RESULTS ################# 
-
-experimental_activities_interspec_2024cal = [
-1283.5748064185852
-,9.56701857731642
-,1059.8534636660463
-,109.78571243006984
-,47.354279028165536
-,976.9745223250458
-,564.0136821965442
-,24.055053741543887
-,3925.6667358315044
-,225978.3770850294
-,8.641203390990839
-,1480.475164225852
-,3.545378049582069
-,16.443880490456756
-,141683.77]
-
-experimental_uncertainties_interspec_2024cal = [
-11.505019860224916
-,1.0468470175786961
-,15.256182909717403
-,1.2092403652867434
-,5.588082663030442
-,158.1728263407352
-,4.923370766254835
-,1.7118933951037367
-,35.22751978312062
-,1950.6765618229635
-,0.12161693661394514
-,15.892505341164915
-,0.1323696495059722
-,0.28527178050358687
-,675.36]
-
-# ROOT activities
-
-experimental_activities = [
-1277.7785213432978
-,9.860120003910335
-,970.0399189999137
-,108.67576641718234 
-,44.41869640318654 
-,940.0388828554179
-,568.8332961651257
-,24.41799508342217
-,3964.671247446269 # copied from interspec 
-,224230.89280539454 # copied from interspec
-,8.56731053307205 # copied from interspec
-,2322.42753
-,3.5299517227162998
-,16.243051418817277
-,199358.77] 
-
-experimental_uncertainties = [
-11.553077365500107
-,1.1389062662744531
-,9.474808511161948
-,1.1937521218344695
-,6.396292282058862
-,186.79684862084045
-,5.0104487226979995
-,1.7894984576503246
-,35.577532226102235
-,1935.592036168745
-,0.11740388508283918
-,99.925
-,0.1575763183253717
-,0.28396943039890343
-,1147.24]
-
-experimental_activities_root2025cal = [
-833.2533802813856
-,7.03617761475889
-,681.7725148143758
-,67.41997744625581
-,27.329763226423605
-,584.2002783190643
-,421.8848933815178
-,16.724490519462055
-,2845.798781973623
-,139925.7137124618
-,5.311350629962082
-,1654.4295112098637 
-,2.5418366080853607
-,10.45693352168561
-,141683.77] 
-
-experimental_uncertainties_root2025cal = [
-7.5338883904035825
-,0.8127230472743654
-,6.6591734005125085
-,0.7405767061402121
-,3.9354859046049997
-,116.0875075953675
-,3.716084553101452
-,1.2256718820409545
-,25.53717359034098
-,1207.8580864953542
-,0.07278517529948038
-,71.18389456363592
-,0.11346706299386194
-,0.18281352310639176
-,675.36]
 
 ################# C/E FUNCTIONS ################# 
 
 def c_over_e(calculated_activities):
     c_over_e_array = []
     for i in np.linspace(0,len(experimental_activities)-1,len(experimental_activities)):
-        c_over_e =  calculated_activities[int(i)]/experimental_activities[int(i)]
+        c_over_e =  (foil_weight_normalisation[int(i)]*calculated_activities[int(i)])/experimental_activities[int(i)]
         c_over_e_array.append(c_over_e)
     return c_over_e_array
 
@@ -458,7 +147,7 @@ ax1.legend(loc="upper left", bbox_to_anchor=(0.02, 0.90),handlelength=0,borderax
 ax2.legend(loc="upper left", bbox_to_anchor=(0.02, 0.98),handlelength=0,borderaxespad=0, frameon=False,fontsize=18, fancybox=False,facecolor='white',framealpha=1)
 ax3.legend(loc="upper left", bbox_to_anchor=(0.02, 0.82),handlelength=0,borderaxespad=0, frameon=False,fontsize=18, fancybox=False,facecolor='white',framealpha=1)
 fig.set_size_inches((17, 6))
-fig.savefig(os.path.join(f"{image_directory}/CE_plots", 'CE_plot_root_sep_foils_test.png'), transparent=False, bbox_inches='tight')
+fig.savefig(os.path.join(f"{working_directory}/ce_plots", 'CE_plot_root_sep_foils_test_int.png'), transparent=False, bbox_inches='tight')
 
 ################# WEIGHTED AVES ################# 
 

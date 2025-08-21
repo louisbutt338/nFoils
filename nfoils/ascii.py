a9db78783eea3360f5eb17de0510d834105511a1
louisbutt338 <louisbutt338@gmail.com>
2025-08-21 17:34:21 +0100

    remove copyright notices now open source

--- nfoils/ascii.py
+++ nfoils/ascii.py
@@ -1,10 +1,4 @@
 """ 
-* Copyright (C) University of Birmingham - All Rights Reserved
-*
-* Unauthorized copying of this file, via any medium is strictly prohibited 
-* Proprietary and confidential
-* Written by Louis Butt LJB841@bham.ac.uk, 20/08/25
-
 module for dealing with ASCII spe files
 """
 

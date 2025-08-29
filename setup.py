a048b5e6a5938bc07834742b14bd709b331c8248
Louis Butt <86600722+louisbutt338@users.noreply.github.com>
2025-08-29 15:06:14 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="nfoils",
+    version="1.0.1",
+    author="Louis Butt",
+    author_email="LJB841@bham.ac.uk",
+    description="Suite of tools for dosimetry foil characterisation of fast neutron sources",
+    url="https://github.com/louisbutt338/nFoils",
+    packages=setuptools.find_packages(),
+    python_requires = '>3.11',
+    install_requires=['numpy', 'matplotlib', 'scipy','sandy', 'actigamma'],
+    classifiers=["Development Status :: 4 - Beta",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: Science/Research",
+                 "Natural Language :: English",
+                 "Topic :: Scientific/Engineering",
+                 "Programming Language :: Python :: 3",
+                 "Operating System :: OS Independent"]
+)
